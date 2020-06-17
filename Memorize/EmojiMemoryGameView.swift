b28ed3429ab29b09dda7e482cdfd5e6bbc61282d
Chris Kim <chris.kim@kareo.com>
2020-06-16 22:14:34 -0700

    ViewBuilder doesn't work

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -32,17 +32,19 @@ struct CardView: View {
         }
     }
     
-    @ViewBuilder
+//    @ViewBuilder
     private func body(for size: CGSize) -> some View {
+        Group {
         if card.isFaceUp || !card.isMatched {
-           return ZStack {
+            ZStack {
                 //Circle().padding(5).opacity(0.4)
                 Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true).padding(5).opacity(0.4)
                 Text(self.card.content)
                     .font(Font.system(size: fontSize(for: size)))
             }
-    //        .modifier(Cardify(isFaceUp: card.isFaceUp))
+//            .modifier(Cardify(isFaceUp: card.isFaceUp))
             .cardify(isFaceUp: card.isFaceUp)
+        }
         }
     }
     
